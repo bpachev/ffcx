ed461c60ab5a20a09e1dc1b295e90cfd4e495323
Benjamin Pachev <benjaminpachev@gmail.com>
2024-11-13 18:18:36 -0600

    Added support for Windows test with NVRTC.

--- demo/nvrtc_test.cpp
+++ demo/nvrtc_test.cpp
@@ -1,15 +1,21 @@
 #include "Components.h"
+#include "FacetIntegrals.h"
+#include "HyperElasticity.h"
+#include "MathFunctions.h"
+#include "StabilisedStokes.h"
+#include "VectorPoisson.h"
 #include "ufcx.h"
 #include "nvrtc.h"
-#include <stdexcept>
 #include <iostream>
-#include <sstream>
+#include <format>
+#include <stdexcept>
 #include <string>
+#include <vector>
 
-int main()
+void check_nvrtc_compilation(ufcx_form* form)
 {
   // extract kernel
-  ufcx_integral* integral = form_Components_L->form_integrals[0];
+  ufcx_integral* integral = form->form_integrals[0];
   ufcx_tabulate_tensor_cuda_nvrtc* kernel = integral->tabulate_tensor_cuda_nvrtc;
   // call kernel to get CUDA-wrapped source code
   int num_program_headers;
@@ -36,9 +42,8 @@ int main()
 
  if (nvrtc_err != NVRTC_SUCCESS) {
     throw std::runtime_error(
-      "nvrtcCreateProgram() failed with " +
-      std::string(nvrtcGetErrorString(nvrtc_err)) + " "
-      "at " + std::string(__FILE__) + ":" + std::to_string(__LINE__));
+      std::format("nvrtcCreateProgram() failed with {} at {}:{}",
+          nvrtcGetErrorString(nvrtc_err), __FILE__, __LINE__));
   }
 
   int num_compile_options = 0;
@@ -52,38 +57,47 @@ int main()
     size_t log_size;
     nvrtc_err = nvrtcGetProgramLogSize(program, &log_size);
     if (nvrtc_err != NVRTC_SUCCESS) {
-      program_log = std::string(
-        "nvrtcGetProgramLogSize() failed with " +
-        std::string(nvrtcGetErrorString(nvrtc_err)) + " "
-        "at " + std::string(__FILE__) + ":" + std::to_string(__LINE__));
+      program_log = std::format(
+        "nvrtcGetProgramLogSize() failed with {} at {}:{}",
+        nvrtcGetErrorString(nvrtc_err), __FILE__, __LINE__);
     } else {
       program_log.resize(log_size);
       nvrtc_err = nvrtcGetProgramLog(
         program, const_cast<char*>(program_log.c_str()));
       if (nvrtc_err != NVRTC_SUCCESS) {
-        program_log = std::string(
-          "nvrtcGetProgramLog() failed with " +
-          std::string(nvrtcGetErrorString(nvrtc_err))) + " "
-          "at " + std::string(__FILE__) + ":" + std::to_string(__LINE__);
+        program_log = std::format(
+          "nvrtcGetProgramLog() failed with {} at {}:{}",
+          nvrtcGetErrorString(nvrtc_err), __FILE__, __LINE__);
       }
       if (log_size > 0)
         program_log.resize(log_size-1);
     }
     nvrtcDestroyProgram(&program);
-
-    std::stringstream ss;
-    ss << "nvrtcCompileProgram() failed with "
-       << nvrtcGetErrorString(nvrtc_compile_err) << "\n"
-       << "CUDA C++ source code:\n"
-       << std::string(60, '-') << "\n"
-       << program_src
-       << std::string(60, '-') << "\n"
-       << "NVRTC compiler log:\n"
-       << std::string(60, '-') << "\n"
-       << program_log << "\n"
-       << std::string(60, '-') << "\n";
-    throw std::runtime_error(ss.str());
+    std::string dashes = std::string(60, '-') + "\n";
+    throw std::runtime_error(
+      std::format(
+         (
+           "nvrtcCompileProgram() failed with {}\n CUDA C++ source code:\n{} {} {} "
+           "NVRTC compiler log:\n {} {} {}"
+         ), nvrtcGetErrorString(nvrtc_compile_err), dashes, program_src, dashes,
+         dashes, program_log, dashes
+      )
+    );
   }
+}
 
+int main()
+{
+  std::vector<ufcx_form*> forms = {
+    form_Components_L,
+    form_FacetIntegrals_a,
+    form_HyperElasticity_a_F, form_HyperElasticity_a_J,
+    form_MathFunctions_a,
+    form_StabilisedStokes_a, form_StabilisedStokes_L,
+    form_VectorPoisson_a, form_VectorPoisson_L  
+  };
+  
+  for (ufcx_form* form : forms) check_nvrtc_compilation(form);
+ 
   return 0;
 }
